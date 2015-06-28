4c41af1eac1a2d68edf105b14395e21222de195a
roundand <francis.norton@gmail.com>
2015-06-28 10:33:35 +0100

    Completed https://developer.android.com/training/basics/firstapp/starting-activity.html

--- app/src/main/java/com/roundand/helloworld/MyActivity.java
+++ app/src/main/java/com/roundand/helloworld/MyActivity.java
@@ -1,12 +1,17 @@
 package com.roundand.helloworld;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 
 
 public class MyActivity extends ActionBarActivity {
+
+    public final static String EXTRA_MESSAGE = "com.roundand.helloWorld.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,5 +39,13 @@ public class MyActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 }
